4a93ad6fe57787ecfbb055a5193e6ea07757b183
Julien Calixte <juliencalixte@gmail.com>
2021-01-23 15:32:27 +0100

    :heavy_plus_sign: (store) add store persistence

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,20 @@
 import { createStore } from 'vuex'
+import VuexPersistence from 'vuex-persist'
+
+const vuexLocal = new VuexPersistence({
+  key: 'bizbiz'
+})
 
 export const store = createStore({
-  state: {},
+  state: {
+    minDate: '',
+    maxDate: '',
+    resources: 1,
+    productivity: 1,
+    offDays: 1
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  plugins: [vuexLocal.plugin]
 })
