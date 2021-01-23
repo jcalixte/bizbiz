6f7bef1a38e6906504f3da4a90420e6714aa868f
Julien Calixte <juliencalixte@gmail.com>
2021-01-23 16:11:51 +0100

    :wrench: (pwa) add main color

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const mainColor = '#3f4fa6'
+const mainColor = '#341f97'
 
 module.exports = {
   pwa: {
@@ -8,7 +8,7 @@ module.exports = {
       skipWaiting: true,
       clientsClaim: true
     },
-    name: '',
+    name: 'bizbiz',
     manifestOptions: {
       background_color: mainColor,
       theme_color: mainColor,
